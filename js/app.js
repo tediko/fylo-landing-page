aa538ed26d8b786c2a48bc56d9693966040941b3
tediko <tediko123@wp.pl>
2020-12-18 12:25:33 +0100

    Added lazyLoadAnimations

--- js/app.js
+++ js/app.js
@@ -3,3 +3,4 @@ import { default as lazyLoadAnimations } from './lazyLoadAnimations.js';
 
 new formValidation('data-form-1', 'data-input-1', 'data-cta-1');
 new formValidation('data-form-2', 'data-input-2', 'data-cta-2');
+new lazyLoadAnimations();
